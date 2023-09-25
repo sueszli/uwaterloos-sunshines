61f28a2b134992bef510897b388c04b60adb0936
Yahya Jabary <jabaryyahya@gmail.com>
2023-09-25 14:45:01 -0400

    update

--- analysis (blocked out of google scholar) copy.js
+++ analysis (blocked out of google scholar) copy.js
@@ -10,16 +10,10 @@ const greenLogEnd = '\x1b[0m'
 
 const main = async () => {
     const SALARIES_PATH = './raw data - salary disclosure/uw salaries merged.csv'
-    const CSRANKING_PATH = './raw data - csranking/csrankings.csv'
+    // const CSRANKING_PATH = './raw data - csranking/csrankings.csv'
 
     const strSAL = fs.readFileSync(SALARIES_PATH, 'utf-8')
     const parsedSAL = strSAL
-        .split('\n')
-        .slice(1)
-        .map((line) => line.split(','))
-
-    const strCSR = fs.readFileSync(CSRANKING_PATH, 'utf-8')
-    const parsedCSR = strCSR
         .split('\n')
         .slice(1)
         .map((line) => line.split(','))
@@ -31,17 +25,25 @@ const main = async () => {
         const salFirstname = salEntry[1].toLowerCase()
         const salName = [salFirstname, salLastname].join(' ').toLowerCase()
 
-        for (let j = 0; j < parsedCSR.length; j++) {
-            const csrEntry = parsedCSR[j]
-            const csrName = csrEntry[0].toLowerCase()
+        // look up names in google scholar
+        const nameQuery = salName.replace(/\./g, '').replace(/ /g, '+')
+        const gScholarQuery = 'https://scholar.google.com/citations?view_op=search_authors&mauthors=' + nameQuery
+        const htmlStr = await axios.get(gScholarQuery).then((r) => r.data)
+        const $ = cheerio.load(htmlStr)
 
-            const matchedLastName = csrName.includes(salLastname)
-            if (!matchedLastName) {
-                // fuck
-            } else {
-                // check if first name matches aswell
-            }
+        // get all h3 with class 'gs_ai_name'
+        const gsAiNames = $('h3.gs_ai_name')
+        if (gsAiNames.length === 0) {
+            log(`${salName}: ${redLogStart}no google scholar entries found${redLogEnd}`)
+            continue
         }
+
+        // log('found:', gsAiNames.length)
+
+        // click on first link
+        // get citations
+        // get h-index
+        // get i10-index
     }
 }
 await main()
