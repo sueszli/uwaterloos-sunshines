80813715ab32a49a60ca8dcb1daf0d608dd83d58
Yahya Jabary <jabaryyahya@gmail.com>
2023-09-25 13:06:08 -0400

    update

---
+++ scrapeSalaries.js
@@ -0,0 +1,7 @@
+/*
+ * sources:
+ *  - 2021: https://uwaterloo.ca/about/accountability/salary-disclosure-2021
+ *  - 2022: https://uwaterloo.ca/about/accountability/salary-disclosure-2022
+ */
+
+console.log('Hello world')
